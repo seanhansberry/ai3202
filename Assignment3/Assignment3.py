ebbf77241bed0f6fece7ebdcdda228c5364581b8
user <sean.hansberry@colorado.edu>
2015-09-16 18:04:16 -0600

    Initial Commit

---
+++ Assignment3/Assignment3.py
@@ -0,0 +1,17 @@
+#Sean Hansberry
+#CSCI 3202
+#Assignment 3
+
+import sys
+import Queue
+
+
+def main(argv):
+	print "It worked!"
+	return
+
+
+
+# main entry point
+if __name__ == "__main__":
+	main(sys.argv)
